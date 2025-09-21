b14837fd957d61cb7bda666b2d3cbe27d0315522
Nicodemus Young <nicodemus.young.13@gmail.com>
2025-09-21 15:21:57 -0500

    Update Configuration.h

--- Build_one/Marlin-2.1.2.5/Marlin-2.1.2.5/Marlin/Configuration.h
+++ Build_one/Marlin-2.1.2.5/Marlin-2.1.2.5/Marlin/Configuration.h
@@ -2250,15 +2250,22 @@
 //
 #define PREHEAT_1_LABEL       "PLA+"
 #define PREHEAT_1_TEMP_HOTEND 200
-#define PREHEAT_1_TEMP_BED     65
+#define PREHEAT_1_TEMP_BED     55
 #define PREHEAT_1_TEMP_CHAMBER 35
 #define PREHEAT_1_FAN_SPEED   255 // Value from 0 to 255
 
-#define PREHEAT_2_LABEL       "ABS"
+#define PREHEAT_2_LABEL       "PETG"
 #define PREHEAT_2_TEMP_HOTEND 240
 #define PREHEAT_2_TEMP_BED     70
 #define PREHEAT_2_TEMP_CHAMBER 35
 #define PREHEAT_2_FAN_SPEED   255 // Value from 0 to 255
+
+#define PREHEAT_2_LABEL       "TPU"
+#define PREHEAT_2_TEMP_HOTEND 240
+#define PREHEAT_2_TEMP_BED     70
+#define PREHEAT_2_TEMP_CHAMBER 35
+#define PREHEAT_2_FAN_SPEED   255 // Value from 0 to 255
+
 
 /**
  * @section nozzle park
