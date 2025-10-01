de9598245fbde140b1ca53bf4e2efb767fec3062
Nicodemus Young <nicodemus.young.13@gmail.com>
2025-09-30 21:08:37 -0500

    Update Configuration.h

--- Build_one/Marlin-2.1.2.5/Marlin-2.1.2.5/Marlin/Configuration.h
+++ Build_one/Marlin-2.1.2.5/Marlin-2.1.2.5/Marlin/Configuration.h
@@ -1513,7 +1513,7 @@
  *     |    [-]    |
  *     O-- FRONT --+
  */
-#define NOZZLE_TO_PROBE_OFFSET { -25, -52.5, -2.78 }
+#define NOZZLE_TO_PROBE_OFFSET { -25, -52.5, -2.89 }
 
 // Most probes should stay away from the edges of the bed, but
 // with NOZZLE_AS_PROBE this can be negative for a wider probing area.
@@ -1993,7 +1993,7 @@
 #if EITHER(AUTO_BED_LEVELING_LINEAR, AUTO_BED_LEVELING_BILINEAR)
 
   // Set the number of grid points per dimension.
-  #define GRID_MAX_POINTS_X 5
+  #define GRID_MAX_POINTS_X 8
   #define GRID_MAX_POINTS_Y GRID_MAX_POINTS_X
 
   // Probe along the Y axis, advancing X after each column
